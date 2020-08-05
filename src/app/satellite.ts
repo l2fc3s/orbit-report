c95c853a7bdb89b80cb0489ad44cd3863dfe3be3
Larry LaMar <lmrlrry@gmail.com>
2020-08-04 23:49:00 -0500

    on step 7

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,10 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning() {
+        return this.type.toLowerCase() === "space debris";
+    }
+
+
 }
